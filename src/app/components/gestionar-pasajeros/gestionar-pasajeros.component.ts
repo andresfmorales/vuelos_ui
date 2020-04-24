97e2ff9487c282c8d9410ceed9954ac3b2d61339
andresfmorales <andres131913@gmail.com>
2020-04-23 21:21:45 -0500

    Se finalizo la internacionalizacion de componentes.

--- src/app/components/gestionar-pasajeros/gestionar-pasajeros.component.ts
+++ src/app/components/gestionar-pasajeros/gestionar-pasajeros.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-gestionar-pasajeros',
-  templateUrl: './gestionar-pasajeros.component.html',
-  styleUrls: ['./gestionar-pasajeros.component.css']
+  selector: "app-gestionar-pasajeros",
+  templateUrl: "./gestionar-pasajeros.component.html",
+  styleUrls: ["./gestionar-pasajeros.component.css"]
 })
 export class GestionarPasajerosComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
